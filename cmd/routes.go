08db2f8c1dfac87877ca254402a15769761a15be
Div Rhino <fancydresspenguin+divrhino@gmail.com>
2022-10-15 11:32:42 +0800

    Create facts

--- cmd/routes.go
+++ cmd/routes.go
@@ -7,4 +7,6 @@ import (
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/", handlers.Home)
+
+	app.Post("/fact", handlers.CreateFact)
 }
--- handlers/facts.go
+++ handlers/facts.go
@@ -1,7 +1,24 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/divrhino/divrhino-trivia/database"
+	"github.com/divrhino/divrhino-trivia/models"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Home(c *fiber.Ctx) error {
 	return c.SendString("Div Rhino Trivia App!")
+}
+
+func CreateFact(c *fiber.Ctx) error {
+	fact := new(models.Fact)
+	if err := c.BodyParser(fact); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	database.DB.Db.Create(&fact)
+
+	return c.Status(200).JSON(fact)
 }
