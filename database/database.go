1f07f703e73c952e08c51c0c30d3f871c3d6e7d1
Div Rhino Dev <65655808+divrhino@users.noreply.github.com>
2022-10-15 11:22:05 +0800

    Step 3 - Adding Postgres service and GORM models (#3)
    * Point to .env file within docker-compose.yml
    * Add "db" service to docker-compose.yml
    * Use env variables within docker-compose.yml
    * Install GORM and its Postgres driver
    * Add GORM model
    * Add JSON struct tags
    * Add GORM struct tags
    * Create global DB variable
    * Open connection to database with dsn string
    * Automigrate our GORM models
    * Set global DB variable to our database created here
    * Call db connection method from func main

---
+++ database/database.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/divrhino/divrhino-trivia/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func ConnectDb() {
+	dsn := fmt.Sprintf(
+		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("running migrations")
+	db.AutoMigrate(&models.Fact{})
+
+	DB = Dbinstance{
+		Db: db,
+	}
+}
---
+++ models/models.go
@@ -0,0 +1,9 @@
+package models
+
+import "gorm.io/gorm"
+
+type Fact struct {
+	gorm.Model
+	Question string `json:"question" gorm:"text;not null;default:null`
+	Answer   string `json:"answer" gorm:"text;not null;default:null`
+}
