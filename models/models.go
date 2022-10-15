b8f4c685191295e4c1a1f72964e26a6fd95af2cd
Div Rhino <fancydresspenguin+divrhino@gmail.com>
2022-10-15 11:16:19 +0800

    Add GORM struct tags

--- models/models.go
+++ models/models.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Fact struct {
 	gorm.Model
-	Question string `json:"question"`
-	Answer   string `json:"answer"`
+	Question string `json:"question" gorm:"text;not null;default:null`
+	Answer   string `json:"answer" gorm:"text;not null;default:null`
 }
