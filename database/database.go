67ad16ef02e8bce73c21a6500fb08a4a3b440792
Div Rhino <fancydresspenguin+divrhino@gmail.com>
2022-10-15 11:18:36 +0800

    Open connection to database with dsn string

--- database/database.go
+++ database/database.go
@@ -1,9 +1,35 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
 
 type Dbinstance struct {
 	Db *gorm.DB
 }
 
 var DB Dbinstance
+
+func ConnectDb() {
+	dsn := fmt.Sprintf(
+		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+}
