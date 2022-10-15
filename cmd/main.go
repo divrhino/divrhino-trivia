5d811a110ef384b5f4b45fc3bc5bcd9d588eb7be
Div Rhino <fancydresspenguin+divrhino@gmail.com>
2022-10-15 11:20:54 +0800

    Call db connection method from func main

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,19 @@
+// cmd/main.go
+
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/divrhino/divrhino-trivia/database"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	database.ConnectDb()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Div Rhino!")
+		return c.SendString("Div Rhino Trivia App!")
 	})
 
 	app.Listen(":3000")
